2b018de25dc3863e5fbf34dbb51345dbcb8b4c56
Emre Can Ayar <emrecan.ayar@hotmail.com>
2024-02-29 13:05:32 +0300

    Popular article improvements completed.

--- src/components/menuPosts/MenuPosts.tsx
+++ src/components/menuPosts/MenuPosts.tsx
@@ -1,76 +1,82 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./menuPosts.module.css";
 import { Link } from "react-router-dom";
-import p1 from "../../assets/p1.jpeg"
+import p1 from "../../assets/p1.jpeg";
+
+import articleStore from "../../stores/article/articleStore";
+import { ArticleListModel } from "../../services/article/dtos/articleListModel";
+import { handleApiError } from "../../helpers/errorHelpers";
+import config from "../../config";
+import { formatDateForDate } from "../../helpers/dateHelper";
+import { Spin } from "antd";
 
 const MenuPosts = ({ withImage }: any) => {
+  const [loading, setLoading] = useState(false);
+  const [popularArticles, setPopularArticles] = useState<ArticleListModel>(
+    {} as ArticleListModel
+  );
+
+  useEffect(() => {
+    fetcPopulerArticles();
+  }, []);
+
+  const fetcPopulerArticles = async () => {
+    setLoading(true);
+    try {
+      let data = await articleStore.getArticlesListByDynamic(
+        { pageIndex: 0, pageSize: 4 },
+        { sort: [{ field: "viewCount", dir: "desc" }], filter: undefined }
+      );
+      setPopularArticles(data);
+    } catch (error) {
+      handleApiError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className={styles.items}>
-      <Link to="/" className={styles.item}>
-        <div className={styles.imageContainer}>
-          <img src={p1} alt="" className={styles.image} />
+      {loading ? (
+        <div className={styles.spinnerContainer}>
+          <Spin style={{ alignContent: "center" }} size="large" />{" "}
         </div>
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.travel}`}>Travel</span>
-          <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
-          </div>
-        </div>
-      </Link>
-      <Link to="/" className={styles.item}>
-        <div className={styles.imageContainer}>
-          <img src={p1} alt="" className={styles.image} />
-        </div>
-
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.culture}`}>
-            Culture
-          </span>
-          <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
-          </div>
-        </div>
-      </Link>
-      <Link to="/" className={styles.item}>
-        <div className={styles.imageContainer}>
-          <img src={p1} alt="" className={styles.image} />
-        </div>
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.food}`}>Food</span>
-          <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
-          </div>
-        </div>
-      </Link>
-      <Link to="/" className={styles.item}>
-        <div className={styles.imageContainer}>
-          <img src={p1} alt="" className={styles.image} />
-        </div>
-        <div className={styles.textContainer}>
-          <span className={`${styles.category} ${styles.fashion}`}>
-            Fashion
-          </span>
-          <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </h3>
-          <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
-          </div>
-        </div>
-      </Link>
+      ) : (
+        <>
+          {popularArticles.items &&
+            popularArticles.items.map((article, index) => {
+              const dynamicClass = styles[`a${index % 6}`];
+              const categoryClass = `${styles.category} ${dynamicClass}`;
+              return (
+                <>
+                  <Link to="/" className={styles.item}>
+                    <div className={styles.imageContainer}>
+                      <img
+                        src={`${config.FILE_BASE_URL}${article.articleUploadedFiles[0].newPath}`}
+                        alt=""
+                        className={styles.image}
+                      />
+                    </div>
+                    <div className={styles.textContainer}>
+                      <span className={categoryClass}>
+                        {article.category.name}
+                      </span>
+                      <h3 className={styles.postTitle}>{article.title}</h3>
+                      <div className={styles.detail}>
+                        <span className={styles.username}>
+                          {article.user.firstName} {article.user.lastName}
+                        </span>
+                        <span className={styles.date}>
+                          {" "}
+                          - {formatDateForDate(article.date)}
+                        </span>
+                      </div>
+                    </div>
+                  </Link>
+                </>
+              );
+            })}
+        </>
+      )}
     </div>
   );
 };
