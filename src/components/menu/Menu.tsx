5c6ec846fa320444e819345b6d0cfb6f893d210a
Emre Can Ayar <emrecan.ayar@hotmail.com>
2024-03-04 22:28:15 +0300

    MenuPost and Menu component update

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -7,13 +7,13 @@ const Menu = () => {
     <div className={styles.container}>
       <h2 className={styles.subtitle}>{"En Sıcak"}</h2>
       <h1 className={styles.title}>En Popüler</h1>
-      <MenuPosts withImage={true} />
+      <MenuPosts withImage={true} type={1} />
       <h2 className={styles.subtitle}>Konuya göre keşfet</h2>
       <h1 className={styles.title}>Kategoriler</h1>
       <MenuCategories />
       <h2 className={styles.subtitle}>Editörün seçtiği</h2>
       <h1 className={styles.title}>Editörün Seçimi</h1>
-      <MenuPosts withImage={true} />
+      <MenuPosts withImage={true} type={2} />
     </div>
   );
 };
--- src/components/menuPosts/MenuPosts.tsx
+++ src/components/menuPosts/MenuPosts.tsx
@@ -1,26 +1,46 @@
 import React, { useEffect, useState } from "react";
 import styles from "./menuPosts.module.css";
 import { Link } from "react-router-dom";
-import p1 from "../../assets/p1.jpeg";
-
 import articleStore from "../../stores/article/articleStore";
 import { ArticleListModel } from "../../services/article/dtos/articleListModel";
 import { handleApiError } from "../../helpers/errorHelpers";
 import config from "../../config";
 import { formatDateForDate } from "../../helpers/dateHelper";
 import { Spin } from "antd";
+import editorArticlePickStore from "../../stores/editorArticlePick/editorArticlePickStore";
+import { EditorArticlePickListModel } from "../../services/editorArticlePick/dtos/editorArticlePickListModel";
 
-const MenuPosts = ({ withImage }: any) => {
+export interface MenuPostsProps {
+  withImage: boolean;
+  type: number;
+}
+
+const MenuPosts = ({ withImage, type }: MenuPostsProps) => {
   const [loading, setLoading] = useState(false);
   const [popularArticles, setPopularArticles] = useState<ArticleListModel>(
     {} as ArticleListModel
   );
+  const [editorChoiceArticles, setEditorChoiceArticles] =
+    useState<EditorArticlePickListModel>({} as EditorArticlePickListModel);
 
   useEffect(() => {
-    fetcPopulerArticles();
-  }, []);
+    fetchMenuPostsData(type);
+  }, [type]);
 
-  const fetcPopulerArticles = async () => {
+  const fetchMenuPostsData = async (type: number) => {
+    switch (type) {
+      case 1:
+        fetchPopulerArticles();
+        break;
+      case 2:
+        fetchEditorChoiceArticles();
+        break;
+      default:
+        throw new Error("Invalid type");
+    }
+  };
+
+  const fetchPopulerArticles = async () => {
     setLoading(true);
     try {
       let data = await articleStore.getArticlesListByDynamic(
@@ -34,6 +54,104 @@ const MenuPosts = ({ withImage }: any) => {
       setLoading(false);
     }
   };
+
+  const fetchEditorChoiceArticles = async () => {
+    setLoading(true);
+    try {
+      let data = await editorArticlePickStore.getListByDynamic(
+        { pageIndex: 0, pageSize: 4 },
+        { sort: [{ field: "createdBy", dir: "desc" }], filter: undefined }
+      );
+      setEditorChoiceArticles(data);
+    } catch (error) {
+      handleApiError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  function PopulerArticle() {
+    return (
+      <>
+        {popularArticles.items &&
+          popularArticles.items.map((article, index) => {
+            const dynamicClass = styles[`a${index % 6}`];
+            const categoryClass = `${styles.category} ${dynamicClass}`;
+            return (
+              <>
+                <Link to="/" className={styles.item}>
+                  <div className={styles.imageContainer}>
+                    <img
+                      src={`${config.FILE_BASE_URL}${article.articleUploadedFiles[0].newPath}`}
+                      alt=""
+                      className={styles.image}
+                    />
+                  </div>
+                  <div className={styles.textContainer}>
+                    <span className={categoryClass}>
+                      {article.category.name}
+                    </span>
+                    <h3 className={styles.postTitle}>{article.title}</h3>
+                    <div className={styles.detail}>
+                      <span className={styles.username}>
+                        {article.user.firstName} {article.user.lastName}
+                      </span>
+                      <span className={styles.date}>
+                        {" "}
+                        - {formatDateForDate(article.date)}
+                      </span>
+                    </div>
+                  </div>
+                </Link>
+              </>
+            );
+          })}
+      </>
+    );
+  }
+
+  function EditorChoiceArticle() {
+    return (
+      <>
+        {editorChoiceArticles.items &&
+          editorChoiceArticles.items.map((article, index) => {
+            const dynamicClass = styles[`a${index % 6}`];
+            const categoryClass = `${styles.category} ${dynamicClass}`;
+            return (
+              <>
+                <Link to="/" className={styles.item}>
+                  <div className={styles.imageContainer}>
+                    <img
+                      src={`${config.FILE_BASE_URL}${article.article.articleUploadedFiles[0].newPath}`}
+                      alt=""
+                      className={styles.image}
+                    />
+                  </div>
+                  <div className={styles.textContainer}>
+                    <span className={categoryClass}>
+                      {article.article.category.name}
+                    </span>
+                    <h3 className={styles.postTitle}>
+                      {article.article.title}
+                    </h3>
+                    <div className={styles.detail}>
+                      <span className={styles.username}>
+                        {article.user.firstName} {article.user.lastName}
+                      </span>
+                      <span className={styles.date}>
+                        {" "}
+                        - {formatDateForDate(article.article.date)}
+                      </span>
+                    </div>
+                  </div>
+                </Link>
+              </>
+            );
+          })}
+      </>
+    );
+  }
+
   return (
     <div className={styles.items}>
       {loading ? (
@@ -41,41 +159,7 @@ const MenuPosts = ({ withImage }: any) => {
           <Spin style={{ alignContent: "center" }} size="large" />{" "}
         </div>
       ) : (
-        <>
-          {popularArticles.items &&
-            popularArticles.items.map((article, index) => {
-              const dynamicClass = styles[`a${index % 6}`];
-              const categoryClass = `${styles.category} ${dynamicClass}`;
-              return (
-                <>
-                  <Link to="/" className={styles.item}>
-                    <div className={styles.imageContainer}>
-                      <img
-                        src={`${config.FILE_BASE_URL}${article.articleUploadedFiles[0].newPath}`}
-                        alt=""
-                        className={styles.image}
-                      />
-                    </div>
-                    <div className={styles.textContainer}>
-                      <span className={categoryClass}>
-                        {article.category.name}
-                      </span>
-                      <h3 className={styles.postTitle}>{article.title}</h3>
-                      <div className={styles.detail}>
-                        <span className={styles.username}>
-                          {article.user.firstName} {article.user.lastName}
-                        </span>
-                        <span className={styles.date}>
-                          {" "}
-                          - {formatDateForDate(article.date)}
-                        </span>
-                      </div>
-                    </div>
-                  </Link>
-                </>
-              );
-            })}
-        </>
+        <>{type === 1 ? <PopulerArticle /> : <EditorChoiceArticle />}</>
       )}
     </div>
   );
