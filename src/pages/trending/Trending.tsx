09dc30f093c74f14d7619e45c9021152039efb98
Emre Can Ayar <emrecan.ayar@hotmail.com>
2024-04-03 15:45:42 +0300

    The Search mechanism on the TrendArticles page has been removed. Work on it will continue.

--- src/pages/trending/Trending.tsx
+++ src/pages/trending/Trending.tsx
@@ -1,12 +1,14 @@
 import { useCallback, useEffect, useState } from "react";
 import TrendingArticleCard from "../../components/trendingArticleCard/TrendingArticleCard";
-import { Col, Input, Row, Spin } from "antd";
+import { AutoComplete, Col, Input, Row, SelectProps, Spin } from "antd";
 import { ArticleListModel } from "../../services/article/dtos/articleListModel";
 import articleStore from "../../stores/article/articleStore";
 import { handleApiError } from "../../helpers/errorHelpers";
 import _ from "lodash";
 import styles from "./trending.module.css";
-const { Search } = Input;
+import { UserOutlined } from "@ant-design/icons";
+import { CategoryListModel } from "../../services/category/dtos/categoryListModel";
+import categoryStore from "../../stores/category/categoryStore";
 
 const Trending = () => {
   const [trendArticles, setTrendArticles] = useState<ArticleListModel>(
@@ -15,9 +17,16 @@ const Trending = () => {
   const [loading, setLoading] = useState(false);
   const [pageIndex, setPageIndex] = useState(0);
   const [hasMore, setHasMore] = useState(true);
+  const [popularCategories, setPopularCategories] =
+    useState<CategoryListModel>();
+  const [popularArticles, setPopularArticles] = useState<ArticleListModel>(
+    {} as ArticleListModel
+  );
 
   useEffect(() => {
     fetchTrendingArticles();
+    fetchPopulerCategories();
+    fetchPopulerArticles();
   }, []);
 
   // Debounce ile kaydırma olayını optimize et
@@ -39,6 +48,47 @@ const Trending = () => {
     window.addEventListener("scroll", loadMoreData);
     return () => window.removeEventListener("scroll", loadMoreData);
   }, [loadMoreData]);
+
+  // Popüler kategorileri ve makaleleri setOptions ile ayarlayan fonksiyon
+  const setOptionsWithCategoriesAndArticles = () => {
+    const categoryOptions =
+      popularCategories?.items?.map((category) => ({
+        value: category.name,
+        label: (
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            {category.name}
+            <span></span>
+          </div>
+        ),
+      })) || [];
+
+    const articleOptions =
+      popularArticles?.items?.map((article) => ({
+        value: article.title,
+        label: (
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            {article.title}
+            <span></span>
+          </div>
+        ),
+      })) || [];
+
+    setOptions([
+      {
+        label: renderTitle("Popüler Kategoriler"),
+        options: categoryOptions,
+      },
+      {
+        label: renderTitle("Popüler Makaleler"),
+        options: articleOptions,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    // API'den veri çekme işlemleri sonrasında options güncelle
+    setOptionsWithCategoriesAndArticles();
+  }, [popularCategories, popularArticles]); // Bağımlılıklar eklendi
 
   const fetchTrendingArticles = async () => {
     if (loading) return;
@@ -87,6 +137,33 @@ const Trending = () => {
     }
   };
 
+  const fetchPopulerCategories = async () => {
+    try {
+      const data = await categoryStore.getCategoriesListByDynamic(
+        { pageIndex: 0, pageSize: 6 },
+        { filter: { field: "isPopular", operator: "eq", value: "true" } }
+      );
+      if (data && data.items) {
+        setPopularCategories(data);
+      }
+    } catch (error: any) {
+      console.log("Kategoriler yüklenirken bir hata oluştu : ", error);
+      handleApiError(error);
+    }
+  };
+
+  const fetchPopulerArticles = async () => {
+    try {
+      let data = await articleStore.getArticlesListByDynamic(
+        { pageIndex: 0, pageSize: 4 },
+        { sort: [{ field: "viewCount", dir: "desc" }], filter: undefined }
+      );
+      setPopularArticles(data);
+    } catch (error) {
+      handleApiError(error);
+    }
+  };
+
   const renderLoadingIndicator = () => {
     return loading && hasMore ? (
       <div
@@ -102,18 +179,111 @@ const Trending = () => {
     ) : null;
   };
 
+  const renderTitle = (title: string) => (
+    <span>
+      {title}
+      <a
+        style={{ float: "right" }}
+        href="https://www.google.com/search?q=antd"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        daha fazla
+      </a>
+    </span>
+  );
+
+  const renderItem = (title: string, count: number) => ({
+    value: title,
+    label: (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        {title}
+        <span>{count}</span>
+      </div>
+    ),
+  });
+
+  const [options, setOptions] = useState<SelectProps<object>["options"]>([
+    {
+      label: renderTitle("Kategoriler"),
+      options: [
+        renderItem("AntDesign UI FAQ", 60100),
+        renderItem("AntDesign FAQ", 30010),
+      ],
+    },
+    {
+      label: renderTitle("Makaleler"),
+      options: [renderItem("AntDesign design language", 100000)],
+    },
+  ]);
+
+  const getRandomInt = (max: number, min = 0) =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const searchResult = (query: string) =>
+    new Array(getRandomInt(5))
+      .join(".")
+      .split(".")
+      .map((_, idx) => {
+        const category = `${query}${idx}`;
+        return {
+          value: category,
+          label: (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>
+                Found {query} on{" "}
+                <a
+                  href={`https://s.taobao.com/search?q=${query}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {category}
+                </a>
+              </span>
+              <span>{getRandomInt(200, 100)} results</span>
+            </div>
+          ),
+        };
+      });
+
+  // Kullanıcı arama yapınca çağrılacak
+  const handleSearch = (value: string) => {
+    if (value) {
+      // Kullanıcı bir şeyler yazdıysa, dinamik sonuçları göster
+      setOptions(searchResult(value));
+    } else {
+      // Kullanıcı arama kutusunu boşaltırsa, varsayılan seçenekleri geri yükle
+      setOptionsWithCategoriesAndArticles();
+    }
+  };
   return (
     <>
       <div
         style={{ display: "flex", justifyContent: "center", margin: "20px 0" }}
       >
-        <Search
-          placeholder="Makale Ara..."
+        <AutoComplete
+          popupClassName="certain-category-search-dropdown"
+          popupMatchSelectWidth={500}
           style={{ maxWidth: 500, width: "100%" }}
+          options={options}
           size="large"
-        />
+          placeholder="Ara..."
+          onSearch={handleSearch}
+        >
+          <Input.Search size="large" placeholder="Ara..." enterButton />
+        </AutoComplete>
       </div>
-      <Row gutter={[16, 16]} justify="center">
+      <Row gutter={[16, 16]} justify="start">
         {trendArticles.items &&
           trendArticles.items.map((article, index) => (
             <Col
